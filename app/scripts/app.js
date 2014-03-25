cc7f23c39debe6e188151f61479c2dcfe0b77e60
masamune2002 <masamune2002@gmail.com>
2014-03-24 21:56:01 -0500

    App now has AngularJS support and prints the first 5000 stars in a list

---
+++ app/scripts/app.js
@@ -0,0 +1,39 @@
+var app = angular.module('astronomy-demo', []);
+var starRecords;
+
+app.controller('StarController', function($scope, $http) {
+
+	$http({
+      method: 'GET',
+      url: "./app/assets/hygxyz-5000.csv",
+      transformResponse: function(issuelist) {
+        // Transform CSV file into a JSON object
+        var json = $.csv.toObjects(issuelist);
+        return json;
+      },
+      cache: true,
+    })
+    .success(function(issuelist, status) {
+        $scope.stars = issuelist;
+    });
+	
+	$scope.getStarName = function(star)
+   {
+      if (star.ProperName.length != 0) {
+		return star.ProperName;
+	  } else if (star.BayerFlamsteed != 0) {
+		return star.BayerFlamsteed;
+	  } else if (star.Gliese != 0) {
+		return "Gliese " + star.Gliese;
+	  } else if (star.HR.length != 0) {
+		return "HR " + star.HR;
+	  } else if (star.HD.length != 0) {
+		return "HD " + star.HD;
+	  } else if (star.HIP.length != 0) {
+		return "HIP " + star.HIP;
+	  } else {
+		return "UNKNOWN " + star.StarID;
+	  }
+   };
+	
+});
