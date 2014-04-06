df4502ed87d784733841b6e98ab39ed2c0f5f2d8
masamune2002 <masamune2002@gmail.com>
2014-04-05 20:51:23 -0500

    More reorganization. Also, renaming server to astronomy-demo.js

--- astronomy-demo.js
+++ astronomy-demo.js
@@ -2,5 +2,5 @@ var jquery = require("jquery");
 var http = require("http");
 var connect = require('connect');
 connect.createServer(
-    connect.static(__dirname + "/..")
+    connect.static(__dirname + "/app/")
 ).listen(8080);
